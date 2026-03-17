9740487ba05b698a0e40f14401f54efc15012086
Seba1318 <hanessebastian2006@gmail.com>
2026-03-17 23:18:21 +0200

    Styled the movie selector, updated the movies list

--- scripts/data/movies.js
+++ scripts/data/movies.js
@@ -2,55 +2,140 @@ export const movies = [
   {
     id: 1,
     image: "images/movie-posters/movie1.webp",
-    title: "Movie1",
-    rating: 4.5
+    title: "Zodiac",
+    rating: 7.8
   },
+
   {
     id: 2,
     image: "images/movie-posters/movie2.webp",
-    title: "Movie2",
-    rating: 4.5
+    title: "Maze Runner",
+    rating: 7.2
   },
+
   {
     id: 3,
     image: "images/movie-posters/movie3.webp",
-    title: "Movie3",
-    rating: 4.5
+    title: "Maze Runner: Scorch Trials",
+    rating: 6.7
   },
+
   {
     id: 4,
     image: "images/movie-posters/movie4.webp",
-    title: "Movie4",
-    rating: 4.5
+    title: "Maze Runner: The Death Cure",
+    rating: 7.1
   },
+
   {
     id: 5,
     image: "images/movie-posters/movie5.webp",
-    title: "Movie5",
-    rating: 4.5
+    title: "Hunger Games",
+    rating: 7.5
   },
+
   {
     id: 6,
     image: "images/movie-posters/movie6.webp",
-    title: "Movie6",
-    rating: 4.5
+    title: "Hunger Games: Catching Fire",
+    rating: 7.4
   },
+
   {
     id: 7,
     image: "images/movie-posters/movie7.webp",
-    title: "Movie7",
-    rating: 4.5
+    title: "Hunger Games: Mockingjay Part 1",
+    rating: 6.8
   },
+
   {
     id: 8,
     image: "images/movie-posters/movie8.webp",
-    title: "Movie8",
-    rating: 4.5
+    title: "Hunger Games: Mockingjay Part 2",
+    rating: 6.9
   },
+
   {
     id: 9,
     image: "images/movie-posters/movie9.webp",
-    title: "Movie9",
-    rating: 4.5
+    title: "Little Women",
+    rating: 7.9
   },
+
+  {
+    id: 10,
+    image: "images/movie-posters/movie10.webp",
+    title: "10 Things I Hate About You",
+    rating: 7.6
+  },
+
+  {
+    id: 11,
+    image: "images/movie-posters/movie11.webp",
+    title: "Good Will Hunting",
+    rating: 8.3
+  },
+
+  {
+    id: 12,
+    image: "images/movie-posters/movie12.webp",
+    title: "Dead Poets Society",
+    rating: 8.3
+  },
+
+  {
+    id: 13,
+    image: "images/movie-posters/movie13.webp",
+    title: "The Perks of Being a Wallflower",
+    rating: 7.7
+  },
+
+  {
+    id: 14,
+    image: "images/movie-posters/movie14.webp",
+    title: "Anul Nou care n-a fost",
+    rating: 8.1
+  },
+
+  {
+    id: 15,
+    image: "images/movie-posters/movie15.webp",
+    title: "Cinema Paradiso",
+    rating: 8.5
+  },
+
+  {
+    id: 16,
+    image: "images/movie-posters/movie16.webp",
+    title: "Brazii se frâng dar nu se îndoaie",
+    rating: 7.2
+  },
+
+  {
+    id: 17,
+    image: "images/movie-posters/movie17.webp",
+    title: "Made of Honor",
+    rating: 5.9
+  },
+
+  {
+    id: 18,
+    image: "images/movie-posters/movie18.webp",
+    title: "La vita è bella",
+    rating: 8.6
+  },
+
+  {
+    id: 19,
+    image: "images/movie-posters/movie19.webp",
+    title: "Whiplash",
+    rating: 8.5
+  },
+
+  {
+    id: 20,
+    image: "images/movie-posters/movie20.webp",
+    title: "Call Me by Your Name",
+    rating: 8.2
+  }
 ];
--- scripts/mainpage/movies-page.js
+++ scripts/mainpage/movies-page.js
@@ -4,7 +4,10 @@ export function renderMoviesPage(){
   document.querySelector('.js-mainpage-content')
     .innerHTML = `
     <div class="movies-page-container">
-      <div class="movie-selector js-movie-selector"></div>
+      <div class="movie-selector-wrapper">
+        <div class="movie-selector js-movie-selector"></div>
+        <div class="movie-selector-edge"></div>
+      </div>
     </div>
     `;
 
@@ -13,14 +16,14 @@ export function renderMoviesPage(){
   movies.forEach((movie) => {
     moviesPageHTML += `
     <div class="movie-card">
-      <img src=${movie.image} class="movie-card-img">
+      <img src="${movie.image}" class="movie-card-img">
       <div class="movie-card-info">
         <h3>${movie.title}</h3>
-        <p>${movie.rating}</p>
+        <p>Rating: ${movie.rating}</p>
       </div>
     </div>
     `;
   });
 
-  document.querySelector('.js-movie-selector').innerHTML = moviesPageHTML;
+  document.querySelector('.js-movie-selector').innerHTML += moviesPageHTML;
 }
